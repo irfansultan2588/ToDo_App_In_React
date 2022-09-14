023c44d109455024ea97085efe02335edad96ec6
irfan <irfansultan201@gmail.com>
2022-09-14 17:47:25 +0500

    first commit

---
+++ src/Commponents/Todo.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import logo from '../image/Logo.png';
+
+
+
+
+const Todo = () => {
+
+
+    const [inputVal, setInputVal] = useState("")
+    const [eachItem, setEachItems] = useState([])
+
+
+    const handleclick = () => {
+        const myprvItem = [...eachItem, inputVal]
+        setEachItems(myprvItem)
+        setInputVal("")
+    }
+
+    const removeAll = () => {
+        setInputVal("")
+        setEachItems([])
+    }
+    const remove_one = (id) => {
+
+        const remove_item = eachItem.filter((curEle, ind) => {
+            return ind !== id
+        })
+        setEachItems(remove_item)
+
+    }
+
+
+
+    return (
+        <>
+            <div className='main_div'>
+                <img className='logoimg' src={logo} alt="#" />
+            </div>
+            <div className='text_div'><h2>My Todo App</h2></div>
+
+            <div className='input_field'>
+                <input type="text" placeholder='AddItem' onChange={(e) => { setInputVal(e.target.value) }} autoComplete='off' value={inputVal} />
+
+                <div className='btnContainer'>
+                    <button className='btn_style' onClick={handleclick}>Add</button>
+
+                </div>
+            </div>
+
+
+            {eachItem.map((curEle, ind) => {
+                return (
+                    <>
+
+
+                        <div className='showItems'>
+                            <div className='eachItem'>
+                                <div className='showItem'>
+                                    <h3>
+                                        {curEle}
+                                    </h3>
+
+                                    <div>
+                                        <button onClick={() => { remove_one(ind) }} className='btn' >X</button>
+                                    </div>
+
+
+
+
+
+
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                    </>
+                )
+            })}
+            <div className='main_btn'>
+                <button className='btn_styl' onClick={removeAll}>Remove All</button>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Todo
